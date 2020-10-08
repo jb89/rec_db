6eb7fdee1a94cd246d73d8c2efde0f55e1671baa
Jan Burkhardt <jan.burkhardt@capgemini.com>
2020-10-08 17:00:48 +0200

    setup material Tabs with theme

--- rec-db-ui/src/app/app.module.ts
+++ rec-db-ui/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatTabsModule } from '@angular/material/tabs';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatTabsModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
