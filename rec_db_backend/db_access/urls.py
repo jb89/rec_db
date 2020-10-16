7556fe17c23358f6cece46dba928fca6a0eb288d
Jan Burkhardt <jan.burkhardt@capgemini.com>
2020-10-16 09:07:42 +0200

    cleanup backend service

--- rec_db_backend/db_access/urls.py
+++ rec_db_backend/db_access/urls.py
@@ -3,18 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('put_quelle/<str:name>/<str:autor>/', views.put_quelle, name='put_quelle'),
-    path('get_quelle/<str:name>/', views.get_quelle, name='get_quelle'),
     path('get_quellen', views.get_quellen, name='get_quellen'),
-    path('put_rezept/<str:name>/', views.put_rezept, name='put_rezept'),
-    path('get_rezepte', views.get_rezepte, name='get_rezepte'),
-    path('put_zutat/<str:name>/', views.put_zutat, name='put_zutat'),
-    path('get_zutat/<str:name>/', views.get_zutat, name='get_zutat'),
+    path('put_quelle/<str:name>/<str:autor>/', views.put_quelle, name='put_quelle'),
     path('get_zutaten', views.get_zutaten, name='get_zutaten'),
-    path('put_zutat_rezept/<int:zutat_id>/<int:rezept_id>/<str:menge>/', views.put_zutat_rezept, name='put_zutat_rezept'),
-    path('put_rezept_quelle/<int:rezept_id>/<int:quelle_id>/<str:stelle>/', views.put_rezept_quelle, name='put_rezept_quelle'),
+    path('put_zutat/<str:name>/', views.put_zutat, name='put_zutat'),
     path('get_rezepte_for_quelle_and_zutat/<int:quelle_id>/<int:zutat_id>/', views.get_rezepte_for_quelle_and_zutat, name='get_rezepte_for_quelle_and_zutat'),
+    path('get_rezepte', views.get_rezepte, name='get_rezepte'),
+    path('put_rezept/<str:name>/', views.put_rezept, name='put_rezept'),
     path('set_rezepte_for_quelle_and_zutat/<int:rezept_id>/<int:quelle_id>/<int:zutat_id>/<str:stelle>/', views.set_rezepte_for_quelle_and_zutat, name='set_rezepte_for_quelle_and_zutat'),
-
 ]
--- rec_db_backend/db_access/views.py
+++ rec_db_backend/db_access/views.py
@@ -6,25 +6,10 @@ import json
 
 from .models import Zutat, Quelle, Rezept, RezeptQuelle, ZutatRezept
 
-# Create your views here.
-def index(request):
-    z = Zutat(name='asdf')
-    z.save()
-
-    # return HttpResponse(Zutat.objects.all())
-    return HttpResponse('beschde')
-
 def put_quelle(request, name, autor):
     q = Quelle(name=name, autor=autor)
     q.save()
     return JsonResponse(model_to_dict(q))
-
-def get_quelle(request, name):
-    try:
-        q = Quelle.objects.get(name=name)
-        return JsonResponse(model_to_dict(q))
-    except Quelle.DoesNotExist:
-        raise Http404("Does not exist")
 
 def get_quellen(request):
     try:
@@ -51,27 +36,12 @@ def put_zutat(request, name):
     z.save()
     return JsonResponse(model_to_dict(z))
 
-def get_zutat(request, name):
-    try:
-        z = Zutat.objects.get(name=name)
-        return JsonResponse(model_to_dict(z))
-    except Zutat.DoesNotExist:
-        raise Http404("Does not exist")
-
 def get_zutaten(request):
     try:
         data = list(Zutat.objects.values())
         return JsonResponse(data, safe=False)
     except Zutat.DoesNotExist:
         raise Http404("Does not exist")
-
-def put_zutat_rezept(request, zutat_id, rezept_id, menge):
-    # Todo
-    return HttpResponse()
-
-def put_rezept_quelle(request, rezept_id, quelle_id, stelle):
-    # Todo
-    return HttpResponse()
 
 def get_rezepte_for_quelle_and_zutat(request, quelle_id, zutat_id):
     qry2 = 'select r.id as rezeptId, r.name as rezeptName, rq.stelle as stelle \
