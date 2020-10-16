d6a075162fdce73878c652dfd5228faee0fd5a01
Jan Burkhardt <jan.burkhardt@capgemini.com>
2020-10-16 13:09:56 +0200

    make names unique

--- rec_db_backend/db_access/models.py
+++ rec_db_backend/db_access/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Zutat(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
 
     def __str__(self):
             return self.name
 
 class Quelle(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     autor = models.CharField(max_length=20)
     def __str__(self):
             return self.name
 
 class Rezept(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     def __str__(self):
             return self.name
 
