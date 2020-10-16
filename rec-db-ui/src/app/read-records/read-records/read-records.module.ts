3dde662e74add07491e813f462f684edb2f7f727
Jan Burkhardt <jan.burkhardt@capgemini.com>
2020-10-16 21:52:34 +0200

    display rezepte for quelle for zutat

--- rec-db-ui/src/app/read-records/read-records/read-records.module.ts
+++ rec-db-ui/src/app/read-records/read-records/read-records.module.ts
@@ -1,13 +1,15 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReadRecordsComponent } from './read-records/read-records.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [ReadRecordsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     ReadRecordsComponent
